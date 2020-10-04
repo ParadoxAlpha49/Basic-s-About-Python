e0eae320b157228dc45893ed6d643b4d32030df2
Sazzad Sarker <66535042+ParadoxAlpha49@users.noreply.github.com>
2020-10-04 17:59:06 +0600

    Update unlimited message.py

--- unlimited message.py
+++ unlimited message.py
@@ -2,7 +2,7 @@ import pyautogui
 import time 
 while True:
     time.sleep(1)
-    pyautogui.typewrite("torko koro ken -__-")
+    pyautogui.typewrite("Your message")
     time.sleep(2)
     pyautogui.press('enter')
 
